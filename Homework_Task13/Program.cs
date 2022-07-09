1b6b57829e04f35ce5d02a49081590495cbfa524
OlegZakharov <varahas@mail.ru>
2022-07-09 13:02:03 +0300

    add task13 from homework to seminar2

---
+++ Homework_Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        int number = new Random().Next(10,200);
+        Console.WriteLine(number);
+        string str = Convert.ToString(number);
+        if (str.Length == 3)
+        {
+             Console.WriteLine($"{str[2]}");
+        }
+        else
+        {
+             Console.WriteLine("Третьей цифры нет");
+        }
+        }
+    }
+}
