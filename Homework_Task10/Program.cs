c8d7856eec4c10068a439e41a84e900b5032eed4
OlegZakharov <varahas@mail.ru>
2022-07-09 12:49:46 +0300

    add task10 from homework to seminar2

---
+++ Homework_Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        int number = new Random().Next(100,1000);
+        Console.WriteLine(number);
+        string str = Convert.ToString(number);
+        Console.WriteLine($"{str[1]}");
+        }
+    }
+}
