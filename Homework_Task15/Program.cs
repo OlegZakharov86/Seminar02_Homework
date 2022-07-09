f5c17913381f9f2aad5c9796bfb9463176f6d3ff
OlegZakharov <varahas@mail.ru>
2022-07-09 13:17:22 +0300

    add task15 from homework to seminar2

---
+++ Homework_Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        int number = new Random().Next(1,8);
+        Console.WriteLine(number);
+        if (  number > 5 && number < 8)
+        {
+             Console.WriteLine("Сегодня выходной");
+        }
+        else
+        {
+             Console.WriteLine("Дуй на работу");
+        }
+        }
+    }
+}
+
